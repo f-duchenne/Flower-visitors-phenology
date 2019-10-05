417d968b61031c7c2e69364907484c7dd774f540
François Duchenne <39925114+f-duchenne@users.noreply.github.com>
2019-10-05 17:19:40 +0200

    Script used to estimate phenological shifts

--- script_phenological_shits.r
+++ script_phenological_shits.r
@@ -6,13 +6,13 @@ donnf=as.data.frame(fread("data_set_to_download.txt",sep="\t",header=T)) #see th
 
 donnf$Year=as.numeric(as.character(donnf$Year)) #convert year to a numeric variable if it is not
 
-liste=unique(donnf$Species_gen) # define the speciesand mode liste
+liste=unique(donnf$Species_mode) # define the speciesand mode liste
 
 res=as.data.frame(matrix(NA,1,7))
-names(res)=c("Species_gen","Year_effect","Year_error","Year_pval","Year_var_effect","Year_var_error","Year_var_pval")
+names(res)=c("Species_mode","Year_effect","Year_error","Year_pval","Year_var_effect","Year_var_error","Year_var_pval")
 
 for (i in 1:length(liste)){
-tab=subset(donnf,Species_gen==liste[i,1])
+tab=subset(donnf,Species_gen==liste[i])
 
 #centered spatial variables to make interaction terms more readable 
 tab$Latitude=tab$Latitude-mean(tab$Latitude)
@@ -20,9 +20,9 @@ tab$Longitude=tab$Longitude-mean(tab$Longitude)
 tab$Altitude=tab$Altitude-mean(tab$Altitude)
 
 #AIC Selection on the model for MFD, for some species, omptimize it manually by removing interactions:
-if(liste[i,1]=="Endochironomus albipennis_1" | liste[i,1]=="Endochironomus albipennis_2" | liste[i,1]=="Psodos quadrifaria_NA" | liste[i,1]=="Rhyacia helvetina_NA" |
-liste[i,1]=="Bubas bison_NA" | liste[i,1]=="Colias tyche_NA" | liste[i,1]=="Rheumaptera cervinalis_NA" | liste[i,1]=="Eudonia angustea_2" |
-liste[i,1]=="Onthophagus emarginatus_NA" | liste[i,1]=="Tanypus kraatzi_NA" | liste[i,1]=="Lithophane lamda_NA" | liste[i,1]=="Euonthophagus amyntas_NA"){
+if(liste[i]=="Endochironomus albipennis_1" | liste[i]=="Endochironomus albipennis_2" | liste[i]=="Psodos quadrifaria_NA" | liste[i]=="Rhyacia helvetina_NA" |
+liste[i]=="Bubas bison_NA" | liste[i]=="Colias tyche_NA" | liste[i]=="Rheumaptera cervinalis_NA" | liste[i]=="Eudonia angustea_2" |
+liste[i]=="Onthophagus emarginatus_NA" | liste[i]=="Tanypus kraatzi_NA" | liste[i]=="Lithophane lamda_NA" | liste[i]=="Euonthophagus amyntas_NA"){
 model=lm(Jday~Latitude*Longitude+Altitude+Year,data=tab)
 model=step(model,scope=list(lower=~Latitude+Longitude+Year,upper=model))
 }else{
@@ -78,7 +78,7 @@ a=model2$var$aic
 
 model2$mean$terms=model$terms
 #Keep coefficients in a dataframe defined above
-res[i,1]=as.character(liste[i,1])
+res[i,1]=as.character(liste[i])
 res[i,2]=model2$mean$coef["Year"]
 res[i,3]=summary(model2)$mean$coef["Year",2]
 res[i,4]=summary(model2)$mean$coef["Year",4]
